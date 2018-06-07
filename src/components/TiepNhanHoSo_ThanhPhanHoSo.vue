2a8c45cd9b6a13f7fba4409c001293ba92dafdb9
Trinh Cong Trinh <congtrinh0209@gmail.com>
2018-06-07 15:26:27 +0700

    update

--- src/components/TiepNhanHoSo_ThanhPhanHoSo.vue
+++ src/components/TiepNhanHoSo_ThanhPhanHoSo.vue
@@ -11,13 +11,10 @@
                 <v-card>
                   <v-card-text>
                     <v-layout wrap>
-                      
                       <v-flex xs12 class="text-xs-right">
                         <div :id="'formAlpaca'+item.partNo">
-                          
                         </div>
                       </v-flex>
-
                     </v-layout>
                   </v-card-text>
                 </v-card>
@@ -29,14 +26,14 @@
               </content-placeholders>
               <v-layout row wrap class="flex__checkbox">
                 <v-flex style="width: 300px;" class="layout wrap">
-                  <!-- <v-radio-group v-model="thanhPhanHoSo.dossierTemplates[index].fileType" row>
+                  <v-radio-group v-model="thanhPhanHoSo.dossierTemplates[index].fileType" row>
                     <v-radio label="" value="2"></v-radio>
                     <v-radio label="" value="0"></v-radio>
                     <v-radio label="" value="1"></v-radio>
-                  </v-radio-group> -->
-                 <v-checkbox class="flex" v-model="thanhPhanHoSo.dossierTemplates[index].fileType" value="2"></v-checkbox>
+                  </v-radio-group>
+                 <!-- <v-checkbox class="flex" v-model="thanhPhanHoSo.dossierTemplates[index].fileType" value="2"></v-checkbox>
                  <v-checkbox class="flex" v-model="thanhPhanHoSo.dossierTemplates[index].fileType" value="0"></v-checkbox>
-                 <v-checkbox class="flex" v-model="thanhPhanHoSo.dossierTemplates[index].fileType" value="1"></v-checkbox>
+                 <v-checkbox class="flex" v-model="thanhPhanHoSo.dossierTemplates[index].fileType" value="1"></v-checkbox> -->
                 </v-flex>
                 <v-flex style="width: 150px;" class="text-right">
                   <v-btn icon class="mt-0 ml-0 mr-2">
